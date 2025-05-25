0d66b13934831a3735f7bc37b31517f2f6b574c1
NexonSU <root@nexon.su>
2025-05-25 08:37:07 +0300

    fix format

--- command_download.go
+++ command_download.go
@@ -72,7 +72,7 @@ func Download(bot *gotgbot.Bot, context *ext.Context) error {
 		NoProgress().
 		NoPlaylist().
 		NoOverwrites().
-		Format("best[height<=720][ext=mp4]/bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720]").
+		Format("bestvideo[height<=?720]+bestaudio/best").
 		RecodeVideo("mp4").
 		EmbedMetadata().
 		Output(filePath).
