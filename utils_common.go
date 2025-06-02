f760e64657e7cf44df7a720ffe77cbe92030dc1f
NexonSU <root@nexon.su>
2025-06-02 14:29:58 +0000

    ParseModeHTML

--- utils_common.go
+++ utils_common.go
@@ -528,7 +528,7 @@ func DownloadFile(filepath string, url string) (err error) {
 func ReplyAndRemove(message string, context ext.Context) error {
 	message += "\n\nЭто сообщение самоуничтожится через 30 секунд."
 	message = html.EscapeString(message)
-	sentMessage, err := Bot.SendMessage(context.Message.Chat.Id, message, &gotgbot.SendMessageOpts{ReplyParameters: &gotgbot.ReplyParameters{MessageId: context.Message.MessageId, AllowSendingWithoutReply: true}})
+	sentMessage, err := Bot.SendMessage(context.Message.Chat.Id, message, &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyParameters: &gotgbot.ReplyParameters{MessageId: context.Message.MessageId, AllowSendingWithoutReply: true}})
 	if err != nil {
 		return err
 	}
