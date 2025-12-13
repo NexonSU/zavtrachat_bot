f78cdd5dd4290f9ed75cdbb86fa636ecb2e367e3
NexonSU <root@nexon.su>
2025-12-13 01:00:54 -0500

    change stats link

--- command_stats.go
+++ command_stats.go
@@ -7,5 +7,5 @@ import (
 
 // Reply with stats link
 func StatsLinks(bot *gotgbot.Bot, context *ext.Context) error {
-	return ReplyAndRemove("<a href='https://t.me/zavtrachat_bot/stats'>Webapp</a>\n<a href='https://grafana.nexon.su/d/aef7a25c-3824-4046-8ed3-53ccb5850c9d/zavtrachat?kiosk'>Прямая ссылка</a>", *context)
+	return ReplyAndRemove("<a href='https://t.me/zavtrachat_bot/stats'>Webapp</a>\n<a href='https://zavtrabot.nexon.su/grafana/d/zavtrachatstats?kiosk'>Прямая ссылка</a>", *context)
 }
