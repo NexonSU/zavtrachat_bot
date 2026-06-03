dec7940fb48c507fedb000c00220a3c91814c6ef
NexonSU <root@nexon.su>
2026-06-03 19:50:42 +0300

    redemption

---
+++ command_redemption.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+)
+
+// Revive users on /redemption
+func Redemption(bot *gotgbot.Bot, context *ext.Context) error {
+	if !IsAdminOrModer(context.Message.From.Id) {
+		return KillSender(bot, context)
+	}
+	rows, err := DB.Model(&Stats{}).Select("context_id").Where("stat_type = 3").Order("last_update desc").Limit(1000).Rows()
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	var userID int64
+	text := fmt.Sprintf("✨ %v кастует редемпшн!\n\n", MentionUser(context.EffectiveUser))
+	revivesCount := 0
+	for rows.Next() {
+		rows.Scan(&userID)
+		target, err := Bot.GetChatMember(context.EffectiveChat.Id, userID, nil)
+		if err != nil {
+			continue
+		}
+		if target.GetStatus() == "restricted" {
+			user := target.GetUser()
+			_, err = Bot.RestrictChatMember(context.Message.Chat.Id, user.Id, gotgbot.ChatPermissions{CanSendMessages: true}, &gotgbot.RestrictChatMemberOpts{UntilDate: time.Now().Add(time.Second * time.Duration(60)).Unix()})
+			if err != nil {
+				continue
+			}
+
+			revivesCount++
+
+			text += fmt.Sprintf("%v возродился в чате.\n", UserFullName(&user))
+		}
+	}
+	if revivesCount == 0 {
+		text += "Но никто не воскрес..."
+		_, err = context.EffectiveChat.SendMessage(bot, text, &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, DisableNotification: true})
+		return err
+	}
+	_, err = bot.SendVideo(context.Message.Chat.Id, gotgbot.InputFileByID("CgACAgIAAxkBAAEBP4dqIE0GwitYu8CoX2Lme3RPF69CmQACuZcAAmsTAAFJjP46l_bNQoE6BA"), &gotgbot.SendVideoOpts{ParseMode: gotgbot.ParseModeHTML, Caption: text})
+	return err
+}
