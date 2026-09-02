507ae6e7ed0f26abcc007e98ee30cf4ae8115445
NexonSU <root@nexon.su>
2026-09-02 19:58:38 +0300

    disable tools

--- ai_init.go
+++ ai_init.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/cloudwego/eino-ext/components/model/ollama"
-	"github.com/cloudwego/eino/compose"
 	"github.com/cloudwego/eino/flow/agent/react"
 )
 
@@ -37,32 +36,32 @@ func AiInit() error {
 	if err != nil {
 		return err
 	}
-	AIVisionModel, err = ollama.NewChatModel(context, &ollama.ChatModelConfig{
-		HTTPClient: &http.Client{
-			Transport: &AuthTransport{
-				Header: headers,
-				Base:   http.DefaultTransport,
-			},
-		},
-		BaseURL: Config.AIURL,
-		Model:   Config.AIVisionModel,
-	})
-	if err != nil {
-		return err
-	}
+	// AIVisionModel, err = ollama.NewChatModel(context, &ollama.ChatModelConfig{
+	// 	HTTPClient: &http.Client{
+	// 		Transport: &AuthTransport{
+	// 			Header: headers,
+	// 			Base:   http.DefaultTransport,
+	// 		},
+	// 	},
+	// 	BaseURL: Config.AIURL,
+	// 	Model:   Config.AIVisionModel,
+	// })
+	// if err != nil {
+	// 	return err
+	// }
 
 	//tools request
-	tools, err := GetAiTools()
-	if err != nil {
-		return err
-	}
+	// tools, err := GetAiTools()
+	// if err != nil {
+	// 	return err
+	// }
 
 	AIAgent, err = react.NewAgent(context, &react.AgentConfig{
 		ToolCallingModel: AIToolModel,
-		ToolsConfig: compose.ToolsNodeConfig{
-			Tools:                tools,
-			ToolArgumentsHandler: CheckToolRestrictions,
-		},
+		// ToolsConfig: compose.ToolsNodeConfig{
+		// 	Tools:                tools,
+		// 	ToolArgumentsHandler: CheckToolRestrictions,
+		// },
 	})
 	if err != nil {
 		return err
